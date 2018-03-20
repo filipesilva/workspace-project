f8fc2041e8915397333f0ecd9c850da8755f853e
Filipe Silva <filipematossilva@gmail.com>
2018-03-20 13:19:44 +0000

    use ng-package.json instead of package.json

--- projects/lib/src/public_api.ts
+++ projects/lib/src/public_api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of lib
  */
 
-// We need to export something to avoid the error below
+// TODO: We need to export something to avoid the error below
 // error TS2306: File 'D:/sandbox/workspace-project/projects/lib/public_api.ts' is not a module.
 
+// TODO: when using ng-package.json as project it looks like ng-packagr expect a `src/` folder.
 export const HELLO_PACKAGR = 'I am packaged with ng-packagr!';
