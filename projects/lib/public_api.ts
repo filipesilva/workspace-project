28af319690e060ace62d0b56271debaf83af0e92
Filipe Silva <filipematossilva@gmail.com>
2018-03-20 13:10:14 +0000

    build using ng-packagr builder

--- projects/lib/public_api.ts
+++ projects/lib/public_api.ts
@@ -1,3 +1,8 @@
 /*
  * Public API Surface of lib
  */
+
+// We need to export something to avoid the error below
+// error TS2306: File 'D:/sandbox/workspace-project/projects/lib/public_api.ts' is not a module.
+
+export const HELLO_PACKAGR = 'I am packaged with ng-packagr!';
